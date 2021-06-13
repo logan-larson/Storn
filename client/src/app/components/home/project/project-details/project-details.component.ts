e5576b2b6d01a7bbd4f4f8c6b4fbe71e7eff41a1
Logan Larson <loganlarson20@gmail.com>
2021-06-13 17:52:21 -0500

    Project Details Skeleton

--- client/src/app/components/home/project/project-details/project-details.component.ts
+++ client/src/app/components/home/project/project-details/project-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
 import { ProjectDetails } from 'src/app/models/ProjectDetails';
 
 @Component({
@@ -9,10 +10,25 @@ import { ProjectDetails } from 'src/app/models/ProjectDetails';
 export class ProjectDetailsComponent implements OnInit {
 
   @Input() details: ProjectDetails;
+  selected: boolean = false;
+  showEditProjectDetailsComponent: boolean = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeSelected() {
+    this.selected = !this.selected;
+  }
+
+  editProjectDetails() {
+    this.showEditProjectDetailsComponent = true;
+  }
+
+  closeEditProjectDetails() {
+    this.showEditProjectDetailsComponent = false;
   }
 
 }
