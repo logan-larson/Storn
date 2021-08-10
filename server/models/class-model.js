1dffd3a4532786cd19f6f8145dd1e61d91aac1d3
Logan Larson <loganlarson20@gmail.com>
2021-08-09 20:54:57 -0500

    BE Schema Models: User and npm Update

--- server/models/class-model.js
+++ server/models/class-model.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const classSchema = new Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	githubId: String,
-	name: String,
-	// TEMP projects are strings
-	projects: [String],
+  _id: mongoose.Schema.Types.ObjectId,
+  githubId: String,
+  name: String,
+  // TEMP projects are strings
+  projects: [String],
 });
 
 exports.ClassSchema = classSchema;
--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Class = require('../models/class-model');
 
 const userSchema = new Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	githubId: String,
-	name: String,
-	// TEMP classes are strings
-	classes: [String],
+  _id: mongoose.Schema.Types.ObjectId,
+  githubId: String,
+  name: String,
+  // TEMP preferences is a String
+  preferences: String,
+  classes: [Class.ClassModel],
 });
 
 exports.UserSchema = userSchema;
 
-exports.UserModel = mongoose.model('User', userSchema);
+exports.User = mongoose.model('User', userSchema);
