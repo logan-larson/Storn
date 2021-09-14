ba60330692b62040de08f657a0e18acd1c8ba0cf
logan-larson <loganlarson20@gmail.com>
2021-09-13 21:29:51 -0500

    Changed port to process.env.port for AWS

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('cookie-session');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.port || 5000;
 
 const cookieSecret = 'thisisthecookiesecret';
 
